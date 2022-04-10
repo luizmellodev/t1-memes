e6696db6aef6510abe3b2f4df638cbe521ef42d8
Tiago Villa <tiago_villa@hotmail.com>
2022-04-10 14:59:43 -0300

    Add controller class and service for questions

---
+++ src/main/java/com/br/ages/orientacaobucalbackend/Controllers/QuestionController.java
@@ -0,0 +1,42 @@
+package com.br.ages.orientacaobucalbackend.Controllers;
+
+import com.br.ages.orientacaobucalbackend.Entity.Question;
+import com.br.ages.orientacaobucalbackend.Services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/question")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public List<Question> getQuestions() {
+        return questionService.getQuestions();
+    }
+
+    @PostMapping
+    public void registerNewQuestion(@RequestBody Question question) {
+        questionService.addNewQuestion(question);
+    }
+
+    @DeleteMapping(path = "{questionId}")
+    public void deleteQuestion(@PathVariable("questionId") Long questionId) {
+        questionService.deleteStudent(questionId);
+    }
+
+    @PutMapping(path = "{questionId}")
+    public void updateQuestion(
+            @PathVariable("questionId") Long questionId,
+            @RequestParam(required = true) String text) {
+        questionService.updateQuestion(questionId, text);
+    }
+}
---
+++ src/main/java/com/br/ages/orientacaobucalbackend/Services/QuestionService.java
@@ -0,0 +1,69 @@
+package com.br.ages.orientacaobucalbackend.Services;
+
+import com.br.ages.orientacaobucalbackend.DataAcess.Repository.QuestionRepository;
+import com.br.ages.orientacaobucalbackend.Entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    /**
+     * Get a list of all of the questions
+     * @return
+     */
+    public List<Question> getQuestions() {
+        return questionRepository.findAll();
+    }
+
+    /**
+     * Add a new question
+     * @param question The question to be added
+     */
+    public void addNewQuestion(Question question) {
+        // TODO adicionar possíveis validações
+
+        questionRepository.save(question);
+    }
+
+    /**
+     * Delete a question by id
+     * @param questionId The id of the question to be deleted
+     */
+    public void deleteStudent(Long questionId) {
+        boolean exists = questionRepository.existsById(questionId);
+
+        if(!exists) {
+            throw new IllegalStateException("question with id " + questionId + " does not exist");
+        }
+        questionRepository.deleteById(questionId);
+    }
+
+    /**
+     * Update a question
+     * @param questionId The id of the question to be updated
+     * @param text The updated text of the question
+     */
+    @Transactional
+    public void updateQuestion(Long questionId, String text) {
+        Question question = questionRepository.findById(questionId).orElseThrow(() -> new IllegalStateException(
+                "question with id " + questionId + " does not exist"));
+
+        // TODO adicionar possíveis validações
+
+        if (text != null && text.length() > 0) {
+            question.setText(text);
+        }
+    }
+
+}

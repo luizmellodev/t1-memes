db4b67946cc71d32a34b61a19b54a6be6271ac7d
ArthurFoltz <arthurmarianoff@hotmail.com>
2022-05-06 03:24:17 -0300

    PDF generator attempt #1
    Fixed the PdfWriter has a private acess error and updated pom.xml dependencies.
    
    Used as reference: https://gist.github.com/asifali9145/4eb1b4dab9fba50b2c9ad918d75c1396

--- src/main/java/com/br/ages/orientacaobucalbackend/Controllers/PDFController.java
+++ src/main/java/com/br/ages/orientacaobucalbackend/Controllers/PDFController.java
@@ -1,11 +1,17 @@
 package com.br.ages.orientacaobucalbackend.Controllers;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class PDFController {
 
-      Document document = new Document();
-      PdfWriter aux =new PdfWriter();
+      public void geraPdf() throws DocumentException, IOException {
 
-document.open();
-      Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-      Chunk chunk = new Chunk("Hello World", font);
+            Document document = new Document(PageSize.A4);
+            File file = File.createTempFile("exame", ".pdf");
+            FileOutputStream pdf = new FileOutputStream(file);
+            PdfWriter writer;
 
-document.add(chunk);
-document.close();
+            try {
+                  writer = PdfWriter.getInstance(document, pdf);
+            } catch (DocumentException e) {
+                  throw new RuntimeException("Object creation error.", e);
+            }
+
+            document.open();
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+            Chunk chunk = new Chunk("Hello World", font);
+
+            document.add(chunk);
+            document.close();
+      }
+
 
 
 }

e0b333251ea50f39386106e36062f31fb8a980b9
Lucassilv2 <lucas.032@acad.pucrs.br>
2022-05-21 23:55:05 -0300

    swagger pra todas as controller

--- src/main/java/com/br/ages/orientacaobucalbackend/Config/SwaggerConfig.java
+++ src/main/java/com/br/ages/orientacaobucalbackend/Config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.br.ages.orientacaobucalbackend.Controllers"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
